1ba34f5d73aec77f1a1011b534f39d45f45c096f
Jack <devjack050700@gmail.com>
2024-08-30 23:20:06 +0900

    팀 이름 변경

--- commands/game/teamshuffle.js
+++ commands/game/teamshuffle.js
@@ -14,16 +14,16 @@ module.exports = {
 
     // 팀 나누기
     const teamSize = Math.ceil(shuffledParticipants.length / 2);
-    const team1 = shuffledParticipants.slice(0, teamSize);
-    const team2 = shuffledParticipants.slice(teamSize);
+    const teamBLUE = shuffledParticipants.slice(0, teamSize);
+    const teamRED = shuffledParticipants.slice(teamSize);
 
     // 팀 1 임베드 생성
-    const team1Embed = new EmbedBuilder().setTitle("BLUE").setColor(0x1abc9c).setDescription(team1.join("\n"));
+    const teamBlueEmbed = new EmbedBuilder().setTitle("BLUE").setColor(0x1abc9c).setDescription(teamBLUE.join("\n"));
 
     // 팀 2 임베드 생성
-    const team2Embed = new EmbedBuilder().setTitle("RED").setColor(0xe74c3c).setDescription(team2.join("\n"));
+    const teamRedEmbed = new EmbedBuilder().setTitle("RED").setColor(0xe74c3c).setDescription(teamRED.join("\n"));
 
     // 임베드 메시지 전송
-    await message.channel.send({ embeds: [team1Embed, team2Embed] });
+    await message.channel.send({ embeds: [teamBlueEmbed, teamRedEmbed] });
   },
 };
